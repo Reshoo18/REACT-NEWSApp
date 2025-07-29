a3ebd0bb1f2aa8b016589c9bde361804a393663d
Rajnish Ranjan <ranjanrajnish64@gmail.com>
2025-07-29 16:09:28 +0530

    next button

--- newsapp/src/components/Newsitem.js
+++ newsapp/src/components/Newsitem.js
@@ -8,11 +8,11 @@ export class Newsitem extends Component {
       <div className="my-3">
         
         <div className="card" style={{width: "18rem"}}>
-        <img src={imgUrl} className="card-img-top" alt="..."/>
+        <img src={!imgUrl?"https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fd1e00ek4ebabms.cloudfront.net%2Fproduction%2F8d2bfc5c-d774-4b96-a22f-8b135e0f5db2.jpg?source=next-barrier-page":imgUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}</p>
-    <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">Read More</a>
+    <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark">Read More</a>
   </div>
 </div>
       </div>
