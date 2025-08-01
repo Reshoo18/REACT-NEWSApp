a57212f96b189900991d26f49e29100d489f68f2
Rajnish Ranjan <ranjanrajnish64@gmail.com>
2025-08-01 14:20:36 +0530

    vide 29

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -7,9 +7,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
-
         <Navbar/>
-        <News/>
+        <News pageSize={7} />
       </div>
     )
   }
--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Newsitem from './Newsitem'
+import Spinner from './Spinner';
 
 export class News extends Component {
         
@@ -13,47 +14,56 @@ this.state ={
 }
 }
 async componentDidMount(){
-  let url= "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5aff34f0cb2d405a84e976c0da46f4af&page=1pageSize=20";
+  let url= `https://newsapi.org/v2/top-headlines?country=us&apiKey=5aff34f0cb2d405a84e976c0da46f4af&page=1&pageSize=${this.props.pageSize}`;
+  this.setState({loading : true});
   let data= await fetch(url);
   let parsedData =await data.json();
   console.log(parsedData);
-  this.setState({articles: parsedData.articles,totalResults: parsedData.totalResults})
+  this.setState({articles: parsedData.articles,
+    totalResults: parsedData.totalResults,
+  loading: false})
 }
  
 handlePreviousClick=async()=>{
 
-let url= `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5aff34f0cb2d405a84e976c0da46f4af&page=${this.state.page - 1}&pageSize=20`;
+let url= `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5aff34f0cb2d405a84e976c0da46f4af&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+  this.setState({loading : true});
   let data= await fetch(url);
   let parsedData =await data.json();
    console.log(parsedData);
   this.setState({
   page: this.state.page - 1,
-  articles: parsedData.articles
+  articles: parsedData.articles,
+  loading: false
 })
 }  
 
 handleNextClick=async()=>{
-if(this.state.page +1 >Math.ceil(this.state.totalResults/20)){
+if(!(this.state.page +1 >Math.ceil(this.state.totalResults/this.props.pageSize))){
 
-}
-else{
-let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5aff34f0cb2d405a84e976c0da46f4af&page=${this.state.page + 1}&pageSize=20`;
+
+let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5aff34f0cb2d405a84e976c0da46f4af&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+this.setState({loading : true});
   let data= await fetch(url);
   let parsedData =await data.json();
-   console.log(parsedData);
+  
   this.setState({
   page: this.state.page + 1,
-  articles: parsedData.articles
+  articles: parsedData.articles,
+  loading: false
 })
 }
 }
   render() {
     return (
       <div className="container my-3">
-        <h1>News World -Top Headlines</h1>
+        <h1 className="text-center">News World -Top Headlines</h1>
+        {this.state.loading &&<Spinner/>}
+        
         
         <div className="row">
-          {this.state.articles.map((element)=>{
+
+          {!this.state.loading && this.state.articles.map((element)=>{
             return <div className="col-md-4" key={element.url}>
                      <Newsitem  title={element.title?element.title:""} description ={element.description?element.description:""}  imgUrl={element.urlToImage} newsUrl={element.url}/>
                    </div> 
@@ -61,7 +71,7 @@ let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&api
            </div>
            <div className="container d-flex justify-content-between">
             <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}>&larr; Previous</button>
-            <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+            <button disabled={this.state.page +1 >Math.ceil(this.state.totalResults/this.props.pageSize)}type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
             </div>    
         </div>
       
